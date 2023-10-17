fdf07e4387833775a14711e15a43c717756ea8b6
ajkruk <alicia.j.kruk@gmail.com>
2023-10-16 22:30:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstName = "Alicia"
+let lastName= "Kruk"
+let fullName = firstName + " " + lastName
+console.log(fullName)
+
+let  population = "634464"
+console.log(population)
+console.log(population * 3)
+
+let javaScriptIsCool = true
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+for (i = 1; i <=5; i++) {
+console.log(i)
+}
+
+for (i = 0; i <=4; i++) {
+console.log(i)
+
+}
+
